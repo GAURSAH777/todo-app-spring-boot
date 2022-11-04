fe4e85b723241346d6a11162b3d717b31c621dd1
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-11-04 12:06:16 +0530

    hello bean

--- todo/src/main/java/com/udemy/todo/HelloWorldController.java
+++ todo/src/main/java/com/udemy/todo/HelloWorldController.java
@@ -8,6 +8,10 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
+    }
 
+    @GetMapping("/hello-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
